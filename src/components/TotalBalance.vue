d59b4b1e730a75924d89d496069969db986f0d35
joydumply <nick.babichev@gmail.com>
2023-11-28 12:25:14 +0200

    implemented colors to Total Budget

--- src/components/TotalBalance.vue
+++ src/components/TotalBalance.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="total-value">Balance: {{ total }}</div>
+  <div class="total-value" :class="balanceClass">Balance: {{ total }}</div>
 </template>
 
 <script>
@@ -10,14 +10,34 @@ export default {
       type: Number,
       default: 0
     }
+  },
+  computed: {
+    balanceClass: function () {
+      if (this.total < 0) {
+        return 'red'
+      } else if (this.total > 0) {
+        return 'green'
+      } else {
+        return 'black'
+      }
+    }
   }
 }
 </script>
 <style scoped>
-.total-value{
-    font-size: 26px;
-    text-transform: uppercase;
-    padding: 20px;
-    text-align: center;
+.total-value {
+  font-size: 26px;
+  text-transform: uppercase;
+  padding: 20px;
+  text-align: center;
+}
+.green {
+  color: green;
+}
+.red {
+  color: red;
+}
+.black {
+  color: #000;
 }
 </style>
